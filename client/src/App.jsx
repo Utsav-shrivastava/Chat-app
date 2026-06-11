f742deb4eda4d5b78750a303339b637100015db1
utsav <lmao@gmail.com>
2026-06-11 13:09:31 +0530

    case

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Chat from "./pages/Chat";
+import Chat from "./pages/chat";
 
 function App() {
   return (
